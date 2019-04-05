15eca76852ba751e770c0c71d59f7d24a2ffe776
surajmonde <surajmonde@gmail.com>
2019-04-05 18:32:30 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'chessprogram';
+  chessa1s = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8'];
+
+  chessb2s = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'];
+
+  chessc3s = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'];
+
+  chessd4s = ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'];
+
+  chesse5s = ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8'];
+
+  chessf6s = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8'];
+
+  chessg7s = ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8'];
+
+  chessh8s = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'];
+
+
+
+
+  
+}
